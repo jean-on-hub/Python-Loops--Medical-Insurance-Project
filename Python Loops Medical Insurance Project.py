5f4d8ec97dbd48d16d06d6a73555e76c258f5b0f
jean-on-hub <87473040+jean-on-hub@users.noreply.github.com>
2021-08-11 12:44:47 +0000

    Create Python Loops Medical Insurance Project.py

---
+++ Python Loops Medical Insurance Project.py
@@ -0,0 +1,33 @@
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+# Add your code here
+total_cost = 0
+for a in actual_insurance_costs:
+  total_cost += a
+average_cost = total_cost/len(actual_insurance_costs)
+print("Average Insurance Cost: "+ str(average_cost)+" dollars.")
+for i in range(len(names)):
+  name = names[i]
+  insurance_cost = actual_insurance_costs[i]
+  print ("The insurance cost for "+ name+ " is "+str(insurance_cost)+ " dollars.")
+  if insurance_cost > average_cost:
+    difference = insurance_cost - average_cost
+    print("The insurance cost for " +name+ " is above average by "+ str(difference) + " dollars")
+  elif insurance_cost < average_cost:
+    difference = -insurance_cost + average_cost
+    print("The insurance cost for " +name+ " is below average by "+ str(difference)+ " dollars")
+  else :
+    print("The insurance cost for " +name+ " is equal to average.")
+updated_estimated_costs = [a*11/10 for a in estimated_insurance_costs]
+print(updated_estimated_costs)
+#12
+a = 0
+while a < len(actual_insurance_costs):
+  total_cost += actual_insurance_costs[a]
+  a += 1
+average_cost = total_cost/len(actual_insurance_costs)
+  
+
+print("Average Insurance Cost: "+ str(average_cost)+" dollars.")
